69e45b0c64fce51137aac00005cd397ec2e4093f
KrishnaPermal <Krishna.tantalidespermalnaiken@gmail.com>
2019-10-09 15:27:45 +0400

    second commit

--- cible.php
+++ cible.php
@@ -1,30 +1,58 @@
 <!DOCTYPE html>
 <html lang="fr">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Transmissions de données avec les Formulaires</title>
 </head>
+
 <body>
-    
-    
 
 
-<!-- EXERCICES A TERMINER AVEC LES $_POST[.........] -->
 
 
-<form method="POST" action="cible.php">
+    <!-- A TERMINER AVEC LES $_POST[.........] -->
 
-<label for="nom">Nom : <input type="text" name="nom" size="30" maxlength="55" ></label> <br><br>
 
-<label for="prenom">Prénom : <input type="text" name="prenom" size="30" maxlength="55"></label><br><br>
+    <form method="POST" action="index.php">
 
-<label for="mail">Adresse e-mail : <input type="text" name="mail" size="30" maxlength="55"></label><br><br>
+        <label for="nom">Nom : <input type="text" name="nom" size="30" maxlength="55" value="<?php echo $_POST['name'] ?>"></label> <br><br>
 
-<label for="date of born">Date de naissance : <input type="date" name="date" size="30" maxlength="55"></label><br><br><br><br>
+        <label for="prenom">Prénom : <input type="text" name="prenom" size="30" maxlength="55" value="<?php echo $_POST['firstname'] ?>"></label><br><br>
 
+        <label for="date of born">Date de naissance : <input type="date " name="annif" size="30" maxlength="55" value="<?php echo $_POST['date'] ?>"></label><br><br><br>
+
+        <label for="mail">Adresse e-mail : <input type="text" name="email" size="30" maxlength="55" value="<?php echo $_POST['google'] ?>"></label><br><br>
+
+
+        <select name="choix">
+            <p>
+                <?php echo $_POST['Menu'] ?>
+            </p>
+            <option value="2-4">2-4</option>
+            <option value="4-8">4-8</option>
+            <option value="612">6-12</option>
+        </select><br><br>
+
+
+        
+            <p>
+                <?php echo $_POST['repas'] ?>
+            </p>
+
+        <input type="checkbox" name="menu1" /><label>Poulet sauce d'huître</label><br><br>
+        <input type="checkbox" name="menu2" /><label>Poisson à la chinoise</label><br><br>
+        <input type="checkbox" name="menu3" /><label>Rougail saucisse</label><br><br>
+        <input type="checkbox" name="menu4" /><label>Cabri Massalé</label><br><br>
+        <input type="submit" value="Valider">
+
+
+        <input type="submit" value="Valider">
+
+    </form>
 
 </body>
+
 </html>
-       
--- index.php
+++ index.php
@@ -7,60 +7,59 @@
     <title>Transmissions de données avec les Formulaires</title>
 </head>
 <body>
-    
-
-   <form action="cible.php" method="POST">
-       <p><label>Êtes-vous végétarien? <input type="checkbox" name="vegetarien" /></label></p>
-       <p><input type="submit"></p>
-   </form>
-
 </body>
 </html> -->
 
 
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>EXO FORMULAIRE</title>
 </head>
+
 <body>
 
-<p>MON FORMULAIRE</p><br><br>
+    <p>MON FORMULAIRE</p><br><br>
 
-<form method="POST" action="cible.php">
+    <form method="POST" action="cible.php">
 
-<label for="nom">Nom : <input type="text" name="nom" size="30" maxlength="55"></label> <br><br>
+        <label for="nom">Nom : <input type="text" name="name" size="30" maxlength="55" value="<?php echo $_POST['nom'] ?>"></label><br><br> <!-- Récupère la valeur du name dans cible.php -->
 
-<label for="prenom">Prénom : <input type="text" name="prenom" size="30" maxlength="55"></label><br><br>
+        <label for="prenom">Prénom : <input type="text" name="firstname" size="30" maxlength="55" value="<?php echo $_POST['prenom'] ?>"></label><br><br>
 
-<label for="mail">Adresse e-mail : <input type="text" name="mail" size="30" maxlength="55"></label><br><br>
+        <label for="date of born">Date de naissance : <input type="date" name="date" size="30" maxlength="55" value="<?php echo $_POST['annif'] ?>"></label><br><br>
 
-<label for="date of born">Date de naissance : <input type="date" name="date" size="30" maxlength="55"></label><br><br><br><br>
+        <label for="mail">Adresse e-mail : <input type="text" name="google" size="30" maxlength="55" value="<?php echo $_POST['email'] ?>"></label><br><br>
 
-
-
-
-Nombre de Personnes :
-<select name="Personnes">
-<option selected="selected">2-4</option>
-<option selected="selected">4-8</option>
-<option selected="selected">6-12</option>
-</select><br><br>
-
-Choississez un Menu :<br><br>
-<input type="checkbox" name="menu"/><label>Poulet sauce d'huître</label><br><br>
-<input type="checkbox" name="menu"/><label>Poisson chili</label><br><br>
-<input type="checkbox" name="menu"/><label>Rougail saucisse</label><br><br>
-<input type="checkbox" name="menu"/><label>Cabri Massalé</label><br><br>
-<input type="button" value="Valider">
+        <p>
+            <?php if (isset($_POST['choix'])) { // isset($_POST['choix']) = si 'choix' a une valeur autre que NUL alors execute la condition.
+                echo "tu a choisi " . $_POST['choix'];    
+            } ?>
+        </p>
 
 
-</form>
+        Nombre de Personnes : <br>
+        <select name="Menu"><br>
+            <option value="2-4">2-4</option>
+            <option value="4-8">4-8</option>
+            <option value="6-12">6-12</option>
+        </select><br><br>
+
+        Choississez un Menu :<br><br>
+        name
+        <input type="checkbox" name="repas" /><label>Poulet sauce d'huître</label><br><br>
+        <input type="checkbox" name="repas" /><label>Poisson à la chinoise</label><br><br>
+        <input type="checkbox" name="repas" /><label>Rougail saucisse</label><br><br>
+        <input type="checkbox" name="repas" /><label>Cabri Massalé</label><br><br>
+        <input type="submit" value="Valider">
 
 
+    </form>
 
 </body>
+
 </html>
